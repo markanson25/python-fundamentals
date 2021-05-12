b1aff687f2b3da90aa2798681045e3064a76f2ee
Mark Anson <mark_anson@yahoo.com>
2021-05-12 18:09:27 -0500

    Progress on 5.12.21

--- 03_more_datatypes/4_dictionaries/03_17_first_dict.py
+++ 03_more_datatypes/4_dictionaries/03_17_first_dict.py
@@ -4,3 +4,21 @@ Write a script that creates a dictionary of keys, n and values n*n for numbers 1
 result = {1: 1, 2: 4, 3: 9, ...and so on}
 
 '''
+
+user_input_key_list = []
+user_input_value_list = []
+user_input_key_tuple = ()
+user_input_value_tuple = ()
+my_dictionary = {}
+
+while len(user_input_key_list) < 10:
+    user_input = int(input("Please enter a number: "))
+    user_input_key_list.append(user_input)
+    user_input_key_tuple = tuple(user_input_key_list)
+    user_input_multiplication = user_input ** 2
+    user_input_value_list.append(user_input_multiplication)
+    user_input_value_tuple = tuple(user_input_value_list)
+
+for dictionary_entry in range(0, len(user_input_key_tuple)):
+    my_dictionary[user_input_key_tuple[dictionary_entry]] = user_input_value_tuple[dictionary_entry]
+print(my_dictionary)
--- 03_more_datatypes/4_dictionaries/03_18_occurrence.py
+++ 03_more_datatypes/4_dictionaries/03_18_occurrence.py
@@ -6,3 +6,23 @@ user_input = "hello"
 result = {"h": 1, "e": 1, "l": 2, "o": 1}
 
 '''
+
+user_input_list = []
+user_input_tuple = ()
+user_letter_count = []
+
+user_input = input("Please enter a sentence: ")
+
+for user_input_letter in user_input:
+    user_input_list.append(user_input_letter)
+    # if user_input_letter == ' ':
+    #     user_input_list.remove(' ')
+    user_input_tuple = tuple(user_input_list)
+    letter_count = user_input_list.count(user_input_letter)
+    user_letter_count.append(letter_count)
+    # if letter_count == 0:
+    #     user_letter_count.remove(0)
+
+print(user_input_list)
+print(user_input_tuple)
+print(user_letter_count)
