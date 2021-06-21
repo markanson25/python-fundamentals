e471102df2bd6315d71b240cd23e00b93383133f
Mark Anson <mark_anson@yahoo.com>
2021-06-20 21:38:23 -0500

    finished 08_02_writing

--- 08_file_io/08_02_writing.py
+++ 08_file_io/08_02_writing.py
@@ -2,3 +2,21 @@
 Write a script that reads in the contents of words.txt and writes the contents in reverse
 to a new file words_reverse.txt.
 '''
+
+word_list = []
+
+with open("words.txt", "r") as words:
+    for word in words.readlines():
+        word = word.strip()
+        split = " - "
+        word = word.split(split, 1)[0]
+        word = word.capitalize()
+        word_list.append(word)
+
+word_list.sort(reverse=True)
+
+with open("reverse_words.txt", "w") as reverse_words:
+    reverse_words.write(", \n".join(word_list))
+
+with open("C:/Users/mark_/OneDrive/Documents/CodingNomads/Labs/08_file_io/reverse_words.txt", "r") as reverse_words:
+    print(reverse_words.readlines())
