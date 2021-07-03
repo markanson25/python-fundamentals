807d0fd32483f4e72a46dc502a9ed828d104de18
markanson25 <mark@LAPTOP-A18UGRLD.localdomain>
2021-07-03 11:59:53 -0500

    Finished variable arguments

--- 16_variable_arguments/16_01_args.py
+++ 16_variable_arguments/16_01_args.py
@@ -2,3 +2,12 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+def addition(*args):
+    old_number = 0
+    for number in args:
+        add_input = number + old_number
+        old_number += number
+        print(f"Your answer is: {add_input}")
+
+addition(2, 4, 1, 3)
--- 16_variable_arguments/16_02_kwargs.py
+++ 16_variable_arguments/16_02_kwargs.py
@@ -2,3 +2,14 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+def course_fees(**kwargs):
+    aggregate_fees = 0
+    events_list = []
+    for event, fee in kwargs.items():
+        adding_fees = fee + aggregate_fees
+        events_list.append(event)
+        aggregate_fees += fee
+    print(f"Thank you for attending {events_list}!  The total price for the events is: {adding_fees}")
+
+course_fees(Reading=100, Math=150, History=75)
